0f8503d95679f3c763210c700c52b6fdb3b15af5
liuximei <ximelly@aliyun.com>
2019-06-21 16:32:29 +0800

    引用字体
    引用字体

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import './style.css'
 import headerImg from './headerImg.jpg';
 function createEl(){
     let element=document.createElement('div'),
+        text=document.createElement('p'),
+        text2=document.createElement('p'),
         img=new Image();
-    img.src=headerImg; 
+    img.src=headerImg;
+    text.innerHTML="hello word! 有字体";
+    text2.innerHTML="hello word! 无字体";
     element.appendChild(img);
+    element.appendChild(text);
+    element.appendChild(text2);
     element.classList.add('personInfo');
     return element;
 }
