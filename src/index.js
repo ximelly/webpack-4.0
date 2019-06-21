a3d7318d5115fc0298bbfb87661b57327fa5c14e
liuximei <ximelly@aliyun.com>
2019-06-21 17:35:29 +0800

    新增元素
    新增元素

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-import './style.css'
+import './style.css';
 import headerImg from './headerImg.jpg';
-function createEl(){
-    let element=document.createElement('div'),
-        text=document.createElement('p'),
-        text2=document.createElement('p'),
-        img=new Image();
-    img.src=headerImg;
-    text.innerHTML="hello word! 有字体";
-    text2.innerHTML="hello word! 无字体";
-    element.appendChild(img);
-    element.appendChild(text);
-    element.appendChild(text2);
-    element.classList.add('personInfo');
-    return element;
+import {print,sayHai} from './print.js';
+
+function createEl(type,source,fun){
+    let el;
+    switch(type){
+        case 'btn','p':
+            el=document.createElement(type)
+            el.innerHTML=source;
+            break;
+        case 'img':
+            el=new Image();
+            el.src=source?source:"";
+            break;
+        default:
+            el=document.createElement('div')
+            el.innerHTML=source?source:"";
+    }
+    if(typeof fun =='function'){
+        el.onclick=fun;
+    }
+    return el;
 }
 
-document.body.appendChild(createEl());
+document.body.appendChild(createEl('p','这是一段文本',sayHai));
+document.body.appendChild(createEl('p','这是第二段文本',print));
+document.body.appendChild(createEl('img',headerImg));
+document.body.appendChild(createEl('',"默认文本"));
---
+++ src/print.js
@@ -0,0 +1,7 @@
+function print(){
+    console.log("print")
+}
+function sayHai(){
+    console.log("see you")
+}
+export {print,sayHai}
