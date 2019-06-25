6bec4caa3bec8d1f74dfb74854d71d11f15a049b
liuximei <ximelly@aliyun.com>
2019-06-25 10:29:55 +0800

    热更新
    热更新

--- src/index.js
+++ src/index.js
@@ -24,6 +24,16 @@ function createEl(type,source,fun){
 }
 
 document.body.appendChild(createEl('p','这是一段文本',sayHai));
-document.body.appendChild(createEl('p','这是第二段文本',print));
+let element=createEl('p','这是第二段文本',print);
+document.body.appendChild(element);
 document.body.appendChild(createEl('img',headerImg));
 document.body.appendChild(createEl('',"默认文本"));
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+      console.log('Accepting the updated printMe module!');
+      document.body.removeChild(element);
+      element = createEl('p','这是第二段文本',print); // Re-render the "component" to update the click handler
+      document.body.appendChild(element);
+    })
+}
