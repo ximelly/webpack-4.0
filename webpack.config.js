f81b1ca0dac7327b81dd5200e013eb02d31a7f6c
liuximei <ximelly@aliyun.com>
2019-06-25 09:53:20 +0800

    source map & 观察者模式
    source map & 观察者模式

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry:{
+    app:'./src/index.js',
+    print:'./src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle-[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
